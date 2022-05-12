b64109784f943b8706a4929c7212c78d5804aa3d
bagus nur solayman <solaybagus2@gmail.com>
2022-05-12 15:26:56 +0700

    update height

--- src/views/Frame.vue
+++ src/views/Frame.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-100">
-    <iframe class="w-100" src="https://zoom.kelaspintar.co.id:8080/home?meetingId=89779482681&name=Sulay&passwordMeeting=123456&role=1" frameborder="0"></iframe>
+  <div class="w-100" style="height: 100vh">
+    <iframe class="w-100" style="height: 100vh" src="https://zoom.kelaspintar.co.id:8080/home?meetingId=89779482681&name=Sulay&passwordMeeting=123456&role=1" frameborder="0"></iframe>
   </div>
 </template>
 
